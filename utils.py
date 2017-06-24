bc1f10a5325f45f894ac70782cced291dcd523b2
User Feeeed <feeeed@www1257.sakura.ne.jp>
2017-06-24 19:16:28 +0900

    .

--- utils.py
+++ utils.py
@@ -6,27 +6,6 @@ def team_assign(aochd_team_str):
   aochd_str = _make_aochd_string(team1, team2)
   return aochd_str
 
-def _make_aochd_string(team1, team2):
-  l = []
-  l.append(u"チーム1:")
-  l.append(u"【%s】" % sum([player[1] for player in team1]))
-  for player in team1:
-    l.append(u"%s(%s)" % player)
-  l.append(u"チーム2:")
-  l.append(u"【%s】" % sum([player[1] for player in team2]))
-  for player in team2:
-    l.append(u"%s(%s)" % player)
-  return u" ".join(l)
-
-def _get_players(aochd_team_str):
-  players = []
-  for s in aochd_team_str.split():
-    if "(" in s:
-      name = s.split("(")[0]
-      rate = int(s.split("(")[1].split(")")[0])
-      players.append((name, rate))
-  return players
-
 # 10進数x（自然数）をn進数に変換する関数。拾い物。動作確認済み
 def convert_natural_radix_10_to_n(x, n):
   if x < 0: return None
@@ -42,18 +21,21 @@ def convert_natural_radix_10_to_n(x, n):
 
 # 任意の人数でチーム分け。8人未満対応
 def _team_assign(players):
-  pop = len(players) # 参加人数
+  population = len(players) # 参加人数
 
-  #奇数の場合人数の多い方を理想レートに近づける
-  num_of_digits = pop - int(pop / 2.0)
+  #奇数の場合は人数の多い方を理想レートに近づける
+  num_of_digits = population - int(population / 2.0)
 
   rates = [player[1] for player in players]
   ideal = sum(rates) / 2.0 # 総和の1/2が理想とする
   idx_and_IdealDegree = [] # 組み合わせごとの理想度をメモするリスト
 
-  # pop進数のチーム人数桁まで考えればよい
-  for i in range(pop**num_of_digits):
-    some_idx = [int(s) for s in str(convert_natural_radix_10_to_n(i, pop)).rjust(num_of_digits, "0")]
+  # 「参加人数」進数で「チーム1の人数」桁まで考えればよい
+  # 8人で4人チームなら、0000から7777までのrange
+  # それぞれの桁の数は、ratesのindex
+  # 参加者が奇数なら、人数の多い方がチーム1
+  for i in range(population**num_of_digits):
+    some_idx = [int(s) for s in str(convert_natural_radix_10_to_n(i, population)).rjust(num_of_digits, "0")]
     # そのうち同じ人が同じチームに二度以上入る組み合わせを取り除く
     idx_one_by_one = list(set(some_idx))
     if len(idx_one_by_one) == num_of_digits:
@@ -75,3 +57,24 @@ def _team_assign(players):
   team2.sort(key=lambda player: player[1], reverse=True)
 
   return team1, team2
+
+def _make_aochd_string(team1, team2):
+  l = []
+  l.append(u"チーム1:")
+  l.append(u"【%s】" % sum([player[1] for player in team1]))
+  for player in team1:
+    l.append(u"%s(%s)" % player)
+  l.append(u"チーム2:")
+  l.append(u"【%s】" % sum([player[1] for player in team2]))
+  for player in team2:
+    l.append(u"%s(%s)" % player)
+  return u" ".join(l)
+
+def _get_players(aochd_team_str):
+  players = []
+  for s in aochd_team_str.split():
+    if "(" in s:
+      name = s.split("(")[0]
+      rate = int(s.split("(")[1].split(")")[0])
+      players.append((name, rate))
+  return players
